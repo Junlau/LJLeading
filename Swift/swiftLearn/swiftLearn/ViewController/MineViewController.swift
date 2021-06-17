5929b5269d8aeabd45102323a1e0839cd1c27775
刘军(19054909) <19054909@suning.com>
2021-06-17 11:04:45 +0800

    增加部分页面

---
+++ Swift/swiftLearn/swiftLearn/ViewController/MineViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MinaViewController.swift
+//  swiftLearn
+//
+//  Created by 19054909 on 2021/6/17.
+//
+
+import UIKit
+
+class MineViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "我的"
+    }
+
+}
---
+++ Swift/swiftLearn/swiftLearn/ViewController/SLTabBarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SLTabBarViewController.swift
+//  swiftLearn
+//
+//  Created by 19054909 on 2021/6/17.
+//
+
+import UIKit
+
+class SLTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initViewController()
+    }
+    
+    func initViewController() {
+        let homeTabBar = UITabBarItem(title: "首页", image:  UIImage(named: "home"), selectedImage: UIImage(named: "home-select"))
+        let homeVC = HomeViewController()
+        let homeNav = UINavigationController(rootViewController: homeVC)
+        homeNav.tabBarItem = homeTabBar
+        
+        let discoverTabBar = UITabBarItem(title: "发现", image:  UIImage(named: "discover"), selectedImage: UIImage(named: "discover-select"))
+        let discoverVC =  DiscoverViewController()
+        let discoverNav = UINavigationController(rootViewController: discoverVC)
+        discoverNav.tabBarItem = discoverTabBar
+        
+        let mineTabBar = UITabBarItem(title: "我的", image:  UIImage(named: "mine"), selectedImage: UIImage(named: "mine-select"))
+        let mineVC =  MineViewController()
+        let mineNav = UINavigationController(rootViewController: mineVC)
+        mineNav.tabBarItem = mineTabBar
+        
+        self.setViewControllers([homeNav,discoverNav,mineNav], animated: true)
+    }
+    
+
+}
